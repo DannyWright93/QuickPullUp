c0cbfd8e2670273b91db5079ce8a3c987a4d6bf0
Danny Wright <dannywright93@gmail.com>
2026-02-16 17:43:17 +0100

    Initial Commit

---
+++ QuickPullUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickPullUp
+//
+//  Created by Danny Wright on 16.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickPullUp/QuickPullUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPullUpApp.swift
+//  QuickPullUp
+//
+//  Created by Danny Wright on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct QuickPullUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
